270c342f23bfa2cf438cc2bcfd99652f25021479
skendis <sergei.kendis@gmail.com>
2019-06-20 17:34:45 +0300

    email-preview cmp

--- js/apps/missEmail/cmps/email-preview.cmp.js
+++ js/apps/missEmail/cmps/email-preview.cmp.js
@@ -1,10 +1,10 @@
 export default {
     template: `
-        <div class="email-preview flex align center space-between">
+        <div class="email-preview flex space-between align-center ">
                 <div class="email-subject">{{this.email.subject}}</div>
-                <div>{{formatDate}}</div>
-                <div class="email-controls">
-                    <button class="preview-btn">Delete</button>
+                <div class="email-controls flex ">
+                    <div class="email-date flex align-center">{{formatDate}}</div>
+                    <button class="preview-btn delete-btn">Delete</button>
                     <button class="preview-btn">Mark Read</button>
                 </div>
         </div>
@@ -20,9 +20,13 @@ export default {
     },
     computed: {
         formatDate() {
-            let date = new Date(this.email.sentAt);
-
-            return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+            const date = new Date(this.email.sentAt);
+            const day = date.getDay();
+            const month = date.getMonth();
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            return `${day}/${month}/${year} ${hours > 0 ? hours : '0' + hours}:${minutes > 0 ? minutes : '0' + minutes}`;
         }
     },
     created() {
