524ab4badcdd7a430af71f0a7453c092c60c2184
skendis <sergei.kendis@gmail.com>
2019-06-20 11:14:03 +0300

    router

--- js/pages/homePage.js
+++ js/pages/homePage.js
@@ -1,3 +1,11 @@
 export default {
-    template: `<h1>this is homePage</h1>`
+    template: `
+    <div>
+        <h1>this is homePage</h1>
+        <router-link to="/">home</router-link>
+        <router-link to="/mail">mail</router-link>
+        <router-link to="/book">book</router-link>
+        <router-link to="/notes">notes</router-link>
+    </div>
+    `
 }
