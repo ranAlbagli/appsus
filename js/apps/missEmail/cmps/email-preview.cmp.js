793881bde30bd91f8d613ce0633c81b826b9bd79
skendis <sergei.kendis@gmail.com>
2019-06-20 18:28:57 +0300

    email preview

--- js/apps/missEmail/cmps/email-preview.cmp.js
+++ js/apps/missEmail/cmps/email-preview.cmp.js
@@ -1,11 +1,13 @@
 export default {
     template: `
-        <div class="email-preview flex space-between align-center ">
-                <div class="email-subject">{{this.email.subject}}</div>
+        <div class="email-preview flex space-between align-center" :class="isRead">
+                <div :class="isRead">{{this.email.subject}}</div>
                 <div class="email-controls flex ">
                     <div class="email-date flex align-center">{{formatDate}}</div>
-                    <button class="preview-btn delete-btn">Delete</button>
-                    <button class="preview-btn">Mark Read</button>
+                    <div>
+                        <button @click="emitEmailDelete" class="preview-btn delete-btn"><i class="fas fa-trash-alt"></i></button>
+                        <button @click="emitEmailRead" class="preview-btn">{{this.btnText}}</button>
+                    </div>
                 </div>
         </div>
     `,
@@ -16,7 +18,12 @@ export default {
     },
     props: ['email'],
     methods: {
+        emitEmailRead(){
+            
+        },
+        emitEmailDelete(){
 
+        }
     },
     computed: {
         formatDate() {
@@ -26,7 +33,13 @@ export default {
             const year = date.getFullYear();
             const hours = date.getHours();
             const minutes = date.getMinutes();
-            return `${day}/${month}/${year} ${hours > 0 ? hours : '0' + hours}:${minutes > 0 ? minutes : '0' + minutes}`;
+            return `${day}/${month}/${year} ${hours > 10 ? hours : '0' + hours}:${minutes > 10 ? minutes : '0' + minutes}`;
+        },
+        isRead() {
+            return this.email.isRead ? '' : 'mail-read';
+        },
+        btnText() {
+            return this.email.isRead ? 'Mark Unread' : 'Mark Read'
         }
     },
     created() {
