f3d0dddb136800c0791f86f5d4fa9b6c1c0d3bf7
skendis <sergei.kendis@gmail.com>
2019-06-20 14:04:08 +0300

    email-preview

---
+++ js/apps/missEmail/cmps/email-preview.cmo.js
@@ -0,0 +1,26 @@
+export default {
+    template: `
+        <div class="email-preview">
+                <div class="email-from">{{this.email.from}}</div>
+                <div class="email-subject">{{this.email.subject}}</div>
+                <div class="email-controls">
+                    <button>Delete</button>
+                    <button>Mark Read</button>
+                </div>
+                <div class="email-body">{{this.email.body}}</div>
+        </div>
+    `,
+    data() {
+
+    },
+    props: ['email'],
+    methods: {
+
+    },
+    computed: {
+
+    }
+
+
+
+}
