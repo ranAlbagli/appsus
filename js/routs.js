cb51dcab70789f4e400afba26ba1b99cdb9119ab
skendis <sergei.kendis@gmail.com>
2019-06-22 00:01:56 +0300

    nested -route

--- js/routs.js
+++ js/routs.js
@@ -1,10 +1,16 @@
 //imports
 import homePage from './pages/homePage.js';
 import emailApp from './apps/missEmail/pages/emailApp.cmp.js';
-
+import emailDetails from './apps/missEmail/cmps/email-details.cmp.js'
 
 export default [
     { path: '/', component: homePage },
     { path: '/mail', component: emailApp },
-
+    { path: '/mail/details', component: emailDetails,
+      children: [
+        // UserHome will be rendered inside User's <router-view>
+        // when /user/:id is matched
+        { path: '/', component: emailDetails },
+      ]
+    }
 ]
