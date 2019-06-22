914fb9764adf058ec54be41646e06f53eac53f24
Ran  albagli <albagli.ran@gmail.com>
2019-06-22 13:07:40 +0300

    ///

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -2,15 +2,9 @@ export default {
     template: `<header class="dynamic-header flex align-center space-between">
                 <div class="flex align-center">
                     <div class="logo">appsus</div>
-<<<<<<< HEAD
-                    <div class="search">
-                        <input class="search-input" type="search" name="" id="" placeholder=" Search emails">
-                    </div>
-=======
                     <!-- <div class="search">
                         <input class="search-input" type="search" name="" id="" placeholder="Search emails">
                     </div> -->
->>>>>>> 1c9b6796c4ae8a9d83a94e7300ee5e8e8d01f9cd
                 </div>
                 <nav class="appsus-nav">
                     <ul class="nav-bar clean-list flex">
